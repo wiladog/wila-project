b605e2a32767a778c1da192a909dd0ad52077a10
wiladog <wiladog@gmail.com>
2017-08-23 21:09:53 +0800

    update set title

--- src/libs/util.js
+++ src/libs/util.js
@@ -5,7 +5,7 @@ let util = {
 
 };
 util.title = function(title) {
-    title = title ? title + ' - Home' : 'iView project';
+    title = title ? title + ' - wila' : 'wila project';
     window.document.title = title;
 };
 
